fbbc29e1091b769056388e7befd53212c0e6aafe
Redomeire <redomeire2@gmail.com>
2024-03-01 20:38:00 +0700

    fix: add middleware cors config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,38 @@
 import { NextResponse, NextRequest } from 'next/server'
 import * as jose from "jose"
 
+const corsOptions: {
+  allowedMethods: string[];
+  allowedOrigins: string[];
+  allowedHeaders: string[];
+  exposedHeaders: string[];
+  maxAge?: number;
+  credentials: boolean;
+} = {
+  allowedMethods: (process.env?.ALLOWED_METHODS ?? "").split(","),
+  allowedOrigins: (process.env?.ALLOWED_ORIGIN ?? "").split(","),
+  allowedHeaders: (process.env?.ALLOWED_HEADERS ?? "").split(","),
+  exposedHeaders: (process.env?.EXPOSED_HEADERS ?? "").split(","),
+  maxAge: process.env?.MAX_AGE && parseInt(process.env?.MAX_AGE) || undefined, // 60 * 60 * 24 * 30, // 30 days
+  credentials: process.env?.CREDENTIALS == "true",
+};
+
 export async function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+
+  // Allowed origins check
+  const origin = request.headers.get('origin') ?? '';
+  if (corsOptions.allowedOrigins.includes('*') || corsOptions.allowedOrigins.includes(origin)) {
+    response.headers.set('Access-Control-Allow-Origin', origin);
+  }
+
+  // Set default CORS headers
+  response.headers.set("Access-Control-Allow-Credentials", corsOptions.credentials.toString());
+  response.headers.set("Access-Control-Allow-Methods", corsOptions.allowedMethods.join(","));
+  response.headers.set("Access-Control-Allow-Headers", corsOptions.allowedHeaders.join(","));
+  response.headers.set("Access-Control-Expose-Headers", corsOptions.exposedHeaders.join(","));
+  response.headers.set("Access-Control-Max-Age", corsOptions.maxAge?.toString() ?? "");
+
   if(request.nextUrl.pathname.startsWith('/api/tweet')) {
     const authorization = request.headers.get("Authorization")
   
@@ -27,9 +58,11 @@ export async function middleware(request: NextRequest) {
         status: 401
       })
     }
+    return NextResponse.next()
   }
 
-  return NextResponse.next()
+   // Return
+   return response;
 }
 
 export const config = {
